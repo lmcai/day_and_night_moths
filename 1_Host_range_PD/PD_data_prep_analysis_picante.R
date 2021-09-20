bd1a80314ae577750883e0796c4b01f870de3df4
Liming Cai <34404264+lmcai@users.noreply.github.com>
2021-09-20 16:53:21 -0500

    updates

--- 1_Host_range_PD/PD_data_prep_analysis_picante.R
+++ 1_Host_range_PD/PD_data_prep_analysis_picante.R
@@ -2,14 +2,23 @@
 #INPUT prep
 
 
-y=read.csv('Poan_HOST_combined.picante_input.tsv',row.names=1,stringsAsFactors = F,sep='\t')
+y=read.csv('host_db.picante_input.null.tsv',row.names=1,stringsAsFactors = F,sep='\t')
 x=read.csv('Poan_HOST_combined_genus_nonredundant_matchALLMB.csv')
 
+#Poan's data alone
+#x=read.csv('Poan_Hostrange.csv')
 
+#HOSTS database
+#x=read.csv('HOST_genus_nonredundant_at_genus_level.csv')
 
 #if the butterfly has only one host genus, then mark the second species within this family to use crown group age for PD
 z=read.table('./misc/Poan_HOST_combined.genus_number.tsv',sep='\t',row.names=1,stringsAsFactors = F)
 
+#Poan's data alone
+#z=read.table('./misc/Poan.genus_number.tsv',sep='\t',row.names=1,stringsAsFactors = F)
+
+#HOSTS database alone
+#z=read.table('./misc/HOSTS.genus_number.tsv',sep='\t',row.names=1,stringsAsFactors = F)
 
 for (i in 1:length(x$Host.genus)){
 	y[as.character(x$Butterfly.species.name[i]),paste(as.character(x$Host.genus[i]),'_1',sep='')]=1
@@ -30,7 +39,6 @@ host_recs=read.csv('host_db.picante_input.Poan_HOSTS.csv',row.names = 1)
 
 #PD
 pd.result <- pd(host_recs, sptree, include.root=TRUE)
-#41 species have no host plant recs (lichens, ants, etc.)
 write.table(pd.result,'pd.Poan_HOSTS.tsv',sep='\t')
 
 #MPD nad MNTD
@@ -71,6 +79,91 @@ boxplot(x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==0],x$MPD.z[x$Adult.diel.ac
 
 boxplot(x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==0],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==1],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==2],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,300),main='MNTD')
 
+boxplot(x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==0],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==1],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==2],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(-6,2),main='Normalized MNTD')
+
+par(old.par)
+dev.off()
+
+
+
+
+##########################################
+#Poan's records alone
+##########################################
+library(picante)
+sptree=read.tree('ALLMB.genus_pruned.modified_names.tre')
+host_recs=read.csv('host_db.picante_input.Poan.csv',row.names = 1)
+
+pd.result <- pd(host_recs, sptree, include.root=TRUE)
+write.table(pd.result,'pd.Poan.tsv',sep='\t')
+
+phydist=cophenetic(sptree)
+ses.mpd.result <- ses.mpd(host_recs, phydist, null.model = "taxa.labels",abundance.weighted = FALSE, runs = 199)
+write.table(ses.mpd.result,'mpd.Poan.tsv',sep='\t')
+ses.mntd.result <- ses.mntd(host_recs, phydist, null.model = "taxa.labels",abundance.weighted = FALSE, runs = 199)
+write.table(ses.mntd.result,'mntd.Poan.tsv',sep='\t')
+
+
+x=read.csv('Life_history_traits.csv',row.names = 1,stringsAsFactors = F)
+for (i in 1:length(x$PD)){
+	x$PD[i]=pd.result[rownames(x)[i],'PD']
+	x$Genus_num[i]=pd.result[rownames(x)[i],'SR']
+	x$MPD[i]=ses.mpd.result[rownames(x)[i],'mpd.obs']
+	x$MPD.z[i]=ses.mpd.result[rownames(x)[i],'mpd.obs.z']
+	x$MNTD[i]=ses.mntd.result[rownames(x)[i],'mntd.obs']
+	x$MNTD.z[i]=ses.mntd.result[rownames(x)[i],'mntd.obs.z']
+}
+write.csv(x,'Life_history_traits.Poan.PD_MPD_MNTD.csv')
+
+pdf('PD_analysis_Poan.pdf',width = 5,height = 8)
+old.par <- par(mfrow=c(3, 2))
+boxplot(x$PD[x$Adult.diel.activity..Kawahara.ADA.==0],x$PD[x$Adult.diel.activity..Kawahara.ADA.==1],x$PD[x$Adult.diel.activity..Kawahara.ADA.==2],x$PD[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,3000),main='PD')
+boxplot(x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==0],x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==1],x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==2],x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,50),main='Number of host genera')
+boxplot(x$MPD[x$Adult.diel.activity..Kawahara.ADA.==0],x$MPD[x$Adult.diel.activity..Kawahara.ADA.==1],x$MPD[x$Adult.diel.activity..Kawahara.ADA.==2],x$MPD[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,400),main='MPD')
+boxplot(x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==0],x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==1],x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==2],x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(-8,3),main='Normalized MPD')
+boxplot(x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==0],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==1],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==2],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,300),main='MNTD')
+boxplot(x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==0],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==1],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==2],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(-6,2),main='Normalized MNTD')
+
+par(old.par)
+dev.off()
+
+
+
+##########################################
+#HOSTS databse alone
+##########################################
+library(picante)
+sptree=read.tree('ALLMB.genus_pruned.modified_names.tre')
+host_recs=read.csv('host_db.picante_input.HOSTScsv',row.names = 1)
+
+pd.result <- pd(host_recs, sptree, include.root=TRUE)
+write.table(pd.result,'pd.HOSTS.tsv',sep='\t')
+
+phydist=cophenetic(sptree)
+ses.mpd.result <- ses.mpd(host_recs, phydist, null.model = "taxa.labels",abundance.weighted = FALSE, runs = 199)
+write.table(ses.mpd.result,'mpd.HOSTS.tsv',sep='\t')
+ses.mntd.result <- ses.mntd(host_recs, phydist, null.model = "taxa.labels",abundance.weighted = FALSE, runs = 199)
+write.table(ses.mntd.result,'mntd.HOSTS.tsv',sep='\t')
+
+
+x=read.csv('Life_history_traits.csv',row.names = 1,stringsAsFactors = F)
+for (i in 1:length(x$PD)){
+	x$PD[i]=pd.result[rownames(x)[i],'PD']
+	x$Genus_num[i]=pd.result[rownames(x)[i],'SR']
+	x$MPD[i]=ses.mpd.result[rownames(x)[i],'mpd.obs']
+	x$MPD.z[i]=ses.mpd.result[rownames(x)[i],'mpd.obs.z']
+	x$MNTD[i]=ses.mntd.result[rownames(x)[i],'mntd.obs']
+	x$MNTD.z[i]=ses.mntd.result[rownames(x)[i],'mntd.obs.z']
+}
+write.csv(x,'Life_history_traits.HOSTS.PD_MPD_MNTD.csv')
+
+pdf('PD_analysis_HOSTS.pdf',width = 5,height = 8)
+old.par <- par(mfrow=c(3, 2))
+boxplot(x$PD[x$Adult.diel.activity..Kawahara.ADA.==0],x$PD[x$Adult.diel.activity..Kawahara.ADA.==1],x$PD[x$Adult.diel.activity..Kawahara.ADA.==2],x$PD[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,3000),main='PD')
+boxplot(x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==0],x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==1],x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==2],x$Genus_num[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,50),main='Number of host genera')
+boxplot(x$MPD[x$Adult.diel.activity..Kawahara.ADA.==0],x$MPD[x$Adult.diel.activity..Kawahara.ADA.==1],x$MPD[x$Adult.diel.activity..Kawahara.ADA.==2],x$MPD[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,400),main='MPD')
+boxplot(x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==0],x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==1],x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==2],x$MPD.z[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(-8,3),main='Normalized MPD')
+boxplot(x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==0],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==1],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==2],x$MNTD[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(0,300),main='MNTD')
 boxplot(x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==0],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==1],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==2],x$MNTD.z[x$Adult.diel.activity..Kawahara.ADA.==3],ylim=c(-6,2),main='Normalized MNTD')
 
 par(old.par)
